ec45c1d53fea771347f85d2bc0bc7e1222864cd7
RavenSJx <132763021+RavenSJx@users.noreply.github.com>
2023-05-06 22:25:05 +0800

    Add files via upload

---
+++ emailsent.php
@@ -0,0 +1,18 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message=$_POST["message"];
+
+    echo 'Message Sent';   
+     
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+
+    echo 'Email Sent';    
+}
+
+?>
